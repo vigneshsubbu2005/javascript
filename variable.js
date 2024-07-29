36c685150bcb771a48a8435b6a195fc45a1fdd6d
vigneshsubbu2005 <161560578+vigneshsubbu2005@users.noreply.github.com>
2024-07-29 21:47:19 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,15 @@
+{
+    var a=10;
+    console.log(a);
+    {
+        var b=20;
+        let a=10;
+        console.log(b);
+        console.log(a);
+    }
+    console.log(b);
+    const d="abc";
+    console.log(d);
+    d=10;
+    console.log(d);
+}
