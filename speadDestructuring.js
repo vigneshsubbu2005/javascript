3ebeb4954799280ec794adeea7d903286ac73da3
vigneshsubbu2005 <161560578+vigneshsubbu2005@users.noreply.github.com>
2024-07-29 21:56:07 +0530

    Add files via upload

---
+++ speadDestructuring.js
@@ -0,0 +1,10 @@
+//Destructuring Operator
+/*arr=[10,20,"ABC"]
+var[a,b,c]=arr
+console.log(arr,a,b,c);*/
+
+//spread Operator
+rollno=[10,20,30]
+console.log(rollno);
+latrollno=[10,20,30,40]
+console.log(latrollno);
