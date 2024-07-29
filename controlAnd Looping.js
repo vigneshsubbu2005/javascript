7d37ee6212030957245258559f996bfcb153bfbc
vigneshsubbu2005 <161560578+vigneshsubbu2005@users.noreply.github.com>
2024-07-29 21:49:31 +0530

    Add files via upload

---
+++ controlAnd Looping.js
@@ -0,0 +1,40 @@
+/*var email="info@ramp.in"
+var pwd="123456"
+var usertypingemail="info@ramp.in"
+var usertypingpwd="123456"
+if(usertypingemail==email)
+{
+    if(usertypingpwd==pwd)
+    {
+        console.log("Login Successfully");
+    }
+    else
+    {
+        console.log("Invalid password");
+    }
+}*/
+
+//for in loop
+/*arr=[10,20,"hello",true]
+for(i in arr)
+{
+    console.log(i);
+}*/
+
+
+//for of loop
+/*arr=[10,20,"hello",true]
+for(i of arr)
+{
+console.log(i);
+}*/
+
+
+//for Each loop
+/*arr=[10,20,"hello",true]
+arr.forEach(i => 
+{
+    console.log(i);
+});*/
+
+
