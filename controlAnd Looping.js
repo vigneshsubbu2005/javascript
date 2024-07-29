598f8ebcb909db6c14fef3657f2dcce6821b2533
vigneshsubbu2005 <161560578+vigneshsubbu2005@users.noreply.github.com>
2024-07-29 21:59:22 +0530

    Update controlAnd Looping.js

--- controlAnd Looping.js
+++ controlAnd Looping.js
@@ -1,4 +1,4 @@
-/*var email="info@ramp.in"
+var email="info@ramp.in"
 var pwd="123456"
 var usertypingemail="info@ramp.in"
 var usertypingpwd="123456"
@@ -12,7 +12,7 @@ if(usertypingemail==email)
     {
         console.log("Invalid password");
     }
-}*/
+}
 
 //for in loop
 /*arr=[10,20,"hello",true]
