639563fa210230846d7a468e56d7feb3cc6aab1e
vigneshsubbu2005 <161560578+vigneshsubbu2005@users.noreply.github.com>
2024-07-29 21:49:56 +0530

    Add files via upload

---
+++ otherfunctionTypes.js
@@ -0,0 +1,15 @@
+//1.arrow Function
+
+var votingEligibility = () =>
+{
+    age=2;
+    if(age>=18)
+    {
+        console.log("Eligible for voting");
+    }
+    else
+    {
+        console.log("Not Eligible for voting");
+    }
+}
+votingEligibility()
