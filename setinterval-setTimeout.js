75063ae524f6876e125dfac771124f329eab8205
vigneshsubbu2005 <161560578+vigneshsubbu2005@users.noreply.github.com>
2024-07-29 21:55:50 +0530

    Add files via upload

---
+++ setinterval-setTimeout.js
@@ -0,0 +1,16 @@
+//set timeout
+/*console.log("Set Timeout Example");
+setTimeout (function()
+{
+    console.log("Hello, SKCET");
+},5000)*/
+
+
+//setInterval example
+/*console.log("Set interval example");
+setInterval(function()
+{
+    console.log("Hello, SKCET\nwelcome to setInterval examples");
+},2000)*/
+
+
